b306bebbdb0ae16bcf0aa035459faf72e1909735
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-04-03 09:50:49 +0200

    Update imports and function names

--- src/menu/HistoryInterface.tsx
+++ src/menu/HistoryInterface.tsx
@@ -1,7 +1,8 @@
 import { CharacterModelBase, getCharacterByTag, getDialogueHistory } from '@drincs/pixi-vn';
+import CheckIcon from '@mui/icons-material/Check';
 import CloseIcon from '@mui/icons-material/Close';
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
-import { Box, CssVarsProvider, IconButton, Input, Sheet, Stack, Typography } from "@mui/joy";
+import { Box, Chip, CssVarsProvider, IconButton, Input, Sheet, Stack, Typography } from "@mui/joy";
 import Avatar from '@mui/joy/Avatar';
 import { useState } from 'react';
 
@@ -66,33 +67,65 @@ export default function HistoryInterface() {
                 >
                     <Stack spacing={2} justifyContent="flex-end">
                         {getDialogueHistory()
-                            .map((dialogue) => {
-                                let character = dialogue.characterTag ? getCharacterByTag(dialogue.characterTag) ?? new CharacterModelBase(dialogue.characterTag, { name: dialogue.characterTag }) : undefined
+                            .map((step) => {
+                                let character = step.dialoge?.characterTag ? getCharacterByTag(step.dialoge?.characterTag) ?? new CharacterModelBase(step.dialoge?.characterTag, { name: step.dialoge?.characterTag }) : undefined
                                 return {
                                     character: character?.name ? character.name + (character.surname ? " " + character.surname : "") : undefined,
-                                    text: dialogue.text,
+                                    text: step.dialoge?.text,
                                     icon: character?.icon,
+                                    choices: step.choices?.map((choice) => {
+                                        return {
+                                            text: choice.text,
+                                            isResponse: choice.label === step.choiceMade?.label,
+                                        }
+                                    }),
                                 }
                             })
                             .filter((data) => {
                                 if (!searchString) return true
-                                return data.character?.toLowerCase().includes(searchString.toLowerCase()) || data.text.toLowerCase().includes(searchString.toLowerCase())
+                                return data.character?.toLowerCase().includes(searchString.toLowerCase()) || data.text?.toLowerCase().includes(searchString.toLowerCase())
                             })
                             .map((data, index) => {
-                                return <Stack
-                                    direction="row"
-                                    spacing={1.5}
-                                    key={index}
-                                >
-                                    <Avatar
-                                        size="sm"
-                                        src={data.icon}
-                                    />
-                                    <Box sx={{ flex: 1 }}>
-                                        {data.character && <Typography level="title-sm">{data.character}</Typography>}
-                                        <Typography level="body-sm">{data.text}</Typography>
-                                    </Box>
-                                </Stack>
+                                return <>
+                                    <Stack
+                                        direction="row"
+                                        spacing={1.5}
+                                        key={"text" + index}
+                                    >
+                                        <Avatar
+                                            size="sm"
+                                            src={data.icon}
+                                        />
+                                        <Box sx={{ flex: 1 }}>
+                                            {data.character && <Typography level="title-sm">{data.character}</Typography>}
+                                            <Typography level="body-sm">{data.text}</Typography>
+                                        </Box>
+                                    </Stack>
+                                    <Stack
+                                        direction="row"
+                                        spacing={0.5}
+                                        key={index}
+                                    >
+                                        <Box sx={{ flex: 1 }}>
+                                            {data.choices && data.choices.map((choice, index) => {
+                                                if (choice.isResponse) {
+                                                    return <Chip
+                                                        color="success"
+                                                        endDecorator={<CheckIcon />}
+                                                    >
+                                                        {choice.text}
+                                                    </Chip>
+                                                }
+                                                return <Chip
+                                                    key={"choices" + index}
+                                                    color="primary"
+                                                >
+                                                    {choice.text}
+                                                </Chip>
+                                            })}
+                                        </Box>
+                                    </Stack>
+                                </>
                             })}
                     </Stack>
                 </Box>
