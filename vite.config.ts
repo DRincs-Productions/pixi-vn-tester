c1730d96c8d1c525f2a716d49e016cb61bd728d2
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-04-10 21:33:21 +0200

    Add vite-plugin-pwa to package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import checker from 'vite-plugin-checker'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,33 @@ export default defineConfig({
     react(),
     checker({
       typescript: true,
+    }),
+    VitePWA({
+      registerType: 'prompt',
+      includeAssets: ["vite.svg"],
+      manifest: {
+        name: "Your App",
+        short_name: "your-app",
+        description: "Your App Description",
+        theme_color: '#171717',
+        background_color: '#f0e7db',
+        icons: [
+          {
+            src: '/vite.svg',
+            sizes: '192x192',
+            type: 'image/svg'
+          },
+          {
+            src: '/vite.svg',
+            sizes: '512x512',
+            type: 'image/svg'
+          }
+        ],
+        display: "standalone",
+        scope: '/',
+        start_url: "/",
+        orientation: 'portrait'
+      }
     }),
   ],
 })
