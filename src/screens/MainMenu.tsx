be6b111a2cffc82bfa450bddc1145d54b92b68a8
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-04-27 22:43:51 +0200

    Add hideInterfaceState atom and import it in MainMenu.tsx

--- src/screens/MainMenu.tsx
+++ src/screens/MainMenu.tsx
@@ -3,6 +3,7 @@ import { Grid } from '@mui/joy';
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSetRecoilState } from 'recoil';
+import { hideInterfaceState } from '../atoms/hideInterfaceState';
 import { openSettingsState } from '../atoms/openSettingsState';
 import { reloadInterfaceDataEventState } from '../atoms/reloadInterfaceDataEventState';
 import MenuButton from '../components/MenuButton';
@@ -14,9 +15,11 @@ export default function MainMenu() {
     const navigate = useMyNavigate();
     const setOpenSettings = useSetRecoilState(openSettingsState);
     const notifyLoadEvent = useSetRecoilState(reloadInterfaceDataEventState);
+    const setHideInterface = useSetRecoilState(hideInterfaceState);
     const { t } = useTranslation(["translation"]);
 
     useEffect(() => {
+        setHideInterface(false)
         clearAllGameDatas()
         let bg = addImage("background_main_menu", "https://andreannaking.com/wp-content/uploads/2021/12/Download-Beautiful-Nature-Landscape-Hd-Wallpaper-Full-HD-Wallpapers.jpg")
         bg.load()
