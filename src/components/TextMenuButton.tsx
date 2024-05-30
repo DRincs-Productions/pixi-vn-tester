77814eba533b1b0b3a2c58c4d541a12f0ef4c15d
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-05-30 22:53:15 +0200

    refactor: Remove unused RouterLink import in TextMenuButton component

--- src/components/TextMenuButton.tsx
+++ src/components/TextMenuButton.tsx
@@ -1,5 +1,4 @@
 import { Link, LinkProps, LinkTypeMap, Typography, useTheme } from "@mui/joy";
-import { Link as RouterLink } from "react-router-dom";
 
 interface TextMenuButtonProps extends LinkProps<LinkTypeMap['defaultComponent'], {
     component?: React.ElementType;
@@ -24,7 +23,6 @@ export default function TextMenuButton(props: TextMenuButtonProps) {
                 fontSize: { xs: "0.6rem", sm: "0.7rem", md: "0.8rem", lg: "1rem", xl: "1.1rem" },
                 ...sx
             }}
-            component={RouterLink}
             disabled={disabled}
             {...rest}
         >
