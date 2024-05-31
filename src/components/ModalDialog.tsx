02490a829f6c1e6ac8596d20c9462cec47626344
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-05-31 16:59:33 +0200

    chore: Add Framer Motion library for React animations

--- src/components/ModalDialog.tsx
+++ src/components/ModalDialog.tsx
@@ -1,8 +1,8 @@
 import { DialogActions, DialogContent, Divider, ModalClose } from '@mui/joy';
 import Modal from '@mui/joy/Modal';
 import ModalDialog, { ModalDialogProps } from '@mui/joy/ModalDialog';
-import * as React from 'react';
-import { Transition } from 'react-transition-group';
+import { motion } from "framer-motion";
+import { useEffect, useState } from 'react';
 
 interface ModalDialogCustomProps extends ModalDialogProps {
     open: boolean;
@@ -22,55 +22,68 @@ export default function ModalDialogCustom(props: ModalDialogCustomProps) {
         , sx
         , ...rest
     } = props;
+    const [internalOpen, setInternalOpen] = useState(open)
+    useEffect(() => {
+        if (open) {
+            setInternalOpen(open)
+            return
+        }
+        const timeout = setTimeout(() => {
+            setInternalOpen(open)
+        }, 400)
+        return () => {
+            clearTimeout(timeout)
+        }
+    }, [open])
 
     return (
-        <Transition in={open} timeout={400}>
-            {(state: string) => (
-                <Modal
-                    keepMounted
-                    open={!['exited', 'exiting'].includes(state)}
-                    onClose={() => setOpen(false)}
-                    slotProps={{
-                        backdrop: {
-                            sx: {
-                                opacity: 0,
-                                backdropFilter: 'none',
-                                transition: `opacity 400ms, backdrop-filter 400ms`,
-                                ...{
-                                    entering: { opacity: 1, backdropFilter: 'blur(8px)' },
-                                    entered: { opacity: 1, backdropFilter: 'blur(8px)' },
-                                }[state],
-                            },
-                        },
-                    }}
-                    sx={{
-                        visibility: state === 'exited' ? 'hidden' : 'visible',
-                    }}
-                >
-                    <ModalDialog
-                        sx={{
-                            opacity: 0,
-                            transition: `opacity 300ms`,
-                            ...{
-                                entering: { opacity: 1 },
-                                entered: { opacity: 1 },
-                            }[state],
-                            ...sx,
-                        }}
-                        {...rest}
-                    >
-                        <ModalClose />
-                        {head}
-                        <Divider />
-                        <DialogContent>
-                            {children}
-                        </DialogContent>
-                        {actions && <DialogActions>
-                            {actions}
-                        </DialogActions>}
-                    </ModalDialog>
-                </Modal>
-            )}
-        </Transition>
+        <Modal
+            keepMounted
+            open={internalOpen}
+            onClose={() => setOpen(false)}
+            component={motion.div}
+            initial={{
+                opacity: 0
+            }}
+            animate={{
+                opacity: open ? 1 : 0,
+            }}
+            exit={{
+                opacity: 0
+            }}
+            transition={{
+                duration: 0.4,
+            }}
+        >
+            <ModalDialog
+                sx={{
+                    ...sx,
+                }}
+                component={motion.div}
+                initial={{
+                    opacity: 0,
+                }}
+                animate={{
+                    opacity: open ? 1 : 0,
+                }}
+                exit={{
+                    opacity: 0,
+                }}
+                transition={{
+                    duration: 0.3,
+                }}
+                {...rest}
+            >
+                <ModalClose />
+                {head}
+                <Divider />
+                <DialogContent>
+                    {children}
+                </DialogContent>
+                {actions && <DialogActions>
+                    {actions}
+                </DialogActions>}
+            </ModalDialog>
+        </Modal>
     );
 }
