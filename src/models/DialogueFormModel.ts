28c158a08a00d76c377ff2479ea73afc6cc0a4bd
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-06-04 11:11:29 +0200

    refactor: Remove DialogueFormModel and update components to use InterfaceInfoFormModel

--- src/models/DialogueFormModel.ts
+++
@@ -1,9 +0,0 @@
-import { CharacterBaseModel, ChoiceMenuOptionsType } from "@drincs/pixi-vn";
-
-export interface DialogueFormModel {
-    character: CharacterBaseModel | null,
-    text?: string,
-    menu?: ChoiceMenuOptionsType,
-    showDialogueCard: boolean,
-    showNextButton: boolean,
-}
