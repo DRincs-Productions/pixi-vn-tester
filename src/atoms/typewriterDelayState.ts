06f75f7500ab7829ed2651dea86221522f7f9c85
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-07-20 00:04:08 +0200

    improve

--- src/atoms/typewriterDelayState.ts
+++ src/atoms/typewriterDelayState.ts
@@ -1,6 +1,23 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
-export const typewriterDelayState = atom<number>({
+export const typewriterDelayAtomState = atom<number>({
+    key: 'typewriterDelayAtomState',
+    default: localStorage.getItem('typewriter_delay_millisecond') ? parseInt(localStorage.getItem('typewriter_delay_millisecond')!) : 10,
+});
+
+export const typewriterDelayState = selector<number>({
     key: 'typewriterDelayState',
-    default: localStorage.getItem('typewriter_delay_millisecond') ? parseInt(localStorage.getItem('typewriter_delay_millisecond')!) : 0,
+    get: ({ get }) => {
+        return get(typewriterDelayAtomState)
+    },
+    set: ({ set }, value) => {
+        if (typeof value === "number") {
+            let valueString = value.toString()
+            localStorage.setItem("typewriter_delay_millisecond", valueString)
+        }
+        else {
+            localStorage.removeItem("typewriter_delay_millisecond")
+        }
+        set(typewriterDelayAtomState, value)
+    },
 });
