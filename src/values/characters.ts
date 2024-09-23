26cf319418c9d73e1340f70ee13b17267d5e8633
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-09-23 10:07:42 +0200

    Custom Character

--- src/values/characters.ts
+++ src/values/characters.ts
@@ -1,6 +1,7 @@
-import { CharacterBaseModel, saveCharacter } from "@drincs/pixi-vn";
+import { saveCharacter } from "@drincs/pixi-vn";
+import { Character } from "../models/CharacterModel";
 
-export const juliette = new CharacterBaseModel('___pixivn_juliette___', {
+export const juliette = new Character('___pixivn_juliette___', {
     name: 'Juliette',
     age: 25,
     icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4eMoz7DH8l_Q-iCzSc1xyu_C2iryWh2O9_FcDBpY04w&s",
