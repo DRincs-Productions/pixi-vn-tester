90aff9b5afbb84ef059453935673d7a1176a6e1d
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-11-27 12:10:41 +0100

    Initialize IndexedDB and import ink labels in App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { lazy, Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import LoadingScreen from "./screens/LoadingScreen";
 import { initializeIndexedDB } from "./utilities/indexedDB-utility";
+import { importAllInkLabels } from "./utilities/ink-utility";
 
 export default function App() {
     const Home = lazy(async () => {
         let promileAll = Promise.all([
             initializeIndexedDB(),
+            importAllInkLabels(),
         ])
         await promileAll
         return import('./Home')
