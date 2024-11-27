60707c2abfbe3fdb9ee276dfef88ceb587805862
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-11-27 16:28:05 +0100

    Remove startLabel implementation from labels directory

--- src/labels/startLabel.ts
+++
@@ -1,8 +0,0 @@
-import { narration, newLabel, pixivnTestStartLabel } from "@drincs/pixi-vn";
-
-const startLabel = newLabel("start", [
-    async (props) => {
-        await narration.jumpLabel(pixivnTestStartLabel, props);
-    }
-]);
-export default startLabel;
