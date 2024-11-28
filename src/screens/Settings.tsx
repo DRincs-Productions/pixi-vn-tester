1988f55dd58d0bedcfe486af779e6eeb560795c2
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-11-28 10:33:54 +0100

    Add ReturnMainMenuButton component and integrate it into Settings screen

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -1,14 +1,10 @@
-import ExitToAppIcon from '@mui/icons-material/ExitToApp';
-import { Box, Button, DialogContent, DialogTitle, Divider, Drawer, FormControl, ModalClose, RadioGroup, Sheet, Stack, Typography } from "@mui/joy";
+import { Box, DialogContent, DialogTitle, Divider, Drawer, FormControl, ModalClose, RadioGroup, Sheet, Typography } from "@mui/joy";
 import { Theme, useMediaQuery } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useLocation } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 import { openSettingsState } from '../atoms/openSettingsState';
-import ModalDialogCustom from '../components/ModalDialog';
-import { gameEnd } from '../utilities/actions-utility';
-import { useMyNavigate } from '../utilities/navigate-utility';
+import ReturnMainMenuButton from '../components/ReturnMainMenuButton';
 import AutoSettingToggle from './settings/AutoSettingToggle';
 import DialoguesSettings from './settings/DialoguesSettings';
 import FullScreenSettings from './settings/FullScreenSettings';
@@ -20,9 +16,6 @@ import ThemeSettings from './settings/ThemeSettings';
 
 export default function Settings() {
     const [open, setOpen] = useRecoilState(openSettingsState);
-    const navigate = useMyNavigate();
-    const location = useLocation();
-    const [openYouSure, setOpenYouSure] = useState(false)
     const { t } = useTranslation(["ui"]);
     const smScreen = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
 
@@ -40,128 +33,71 @@ export default function Settings() {
     }
 
     return (
-        <>
-            <Drawer
-                variant="plain"
-                open={open}
-                onClose={() => setOpen(false)}
+        <Drawer
+            variant="plain"
+            open={open}
+            onClose={() => setOpen(false)}
+            sx={{
+                '& .MuiDrawer-content': {
+                    width: smScreen ? '100%' : 600,
+                    maxWidth: '100%',
+                },
+            }}
+            slotProps={{
+                content: {
+                    sx: {
+                        bgcolor: 'transparent',
+                        p: { md: 3, sm: 0 },
+                        boxShadow: 'none',
+                    },
+                },
+            }}
+        >
+            <Sheet
                 sx={{
-                    '& .MuiDrawer-content': {
-                        width: smScreen ? '100%' : 600,
-                        maxWidth: '100%',
-                    },
-                }}
-                slotProps={{
-                    content: {
-                        sx: {
-                            bgcolor: 'transparent',
-                            p: { md: 3, sm: 0 },
-                            boxShadow: 'none',
-                        },
-                    },
+                    borderRadius: 'md',
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: 2,
+                    height: '100%',
+                    overflow: 'auto',
                 }}
             >
-                <Sheet
-                    sx={{
-                        borderRadius: 'md',
-                        p: 2,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        gap: 2,
-                        height: '100%',
-                        overflow: 'auto',
-                    }}
-                >
-                    <DialogTitle>{t("settings")}</DialogTitle>
-                    <ModalClose />
-                    <Divider sx={{ mt: 'auto' }} />
-                    <DialogContent sx={{ gap: 2 }}>
-                        <FormControl>
-                            <RadioGroup>
-                                <Box
-                                    sx={{
-                                        display: 'grid',
-                                        gridTemplateColumns: 'repeat(auto-fill, minmax(140px, 1fr))',
-                                        gap: 1.5,
-                                    }}
-                                >
-                                    <SkipSettingToggle />
-                                    <AutoSettingToggle />
-                                    <OpenHistorySettingButton />
-                                    <SaveLoadSettingButtons />
-                                    <HideInterfaceSettingToggle />
-                                </Box>
-                            </RadioGroup>
-                        </FormControl>
-                        <Typography level="title-md" fontWeight="bold">
-                            {t("dialogues")}
-                        </Typography>
-                        <DialoguesSettings />
-
-                        <Typography level="title-md" fontWeight="bold">
-                            {t("display")}
-                        </Typography>
-                        <FullScreenSettings />
-                        <ThemeSettings />
-                    </DialogContent>
-                    {location.pathname !== '/' && <>
-                        <Divider sx={{ mt: 'auto' }} />
-                        <Stack
-                            direction="row"
-                            justifyContent="space-between"
-                            useFlexGap
-                            spacing={1}
-                        >
-                            <Button
-                                variant="outlined"
-                                color="danger"
-                                startDecorator={<ExitToAppIcon />}
-                                onClick={() => setOpenYouSure(true)}
+                <DialogTitle>{t("settings")}</DialogTitle>
+                <ModalClose />
+                <Divider sx={{ mt: 'auto' }} />
+                <DialogContent sx={{ gap: 2 }}>
+                    <FormControl>
+                        <RadioGroup>
+                            <Box
+                                sx={{
+                                    display: 'grid',
+                                    gridTemplateColumns: 'repeat(auto-fill, minmax(140px, 1fr))',
+                                    gap: 1.5,
+                                }}
                             >
-                                {t("return_main_menu")}
-                            </Button>
-                        </Stack>
-                    </>}
-                </Sheet>
-            </Drawer>
+                                <SkipSettingToggle />
+                                <AutoSettingToggle />
+                                <OpenHistorySettingButton />
+                                <SaveLoadSettingButtons />
+                                <HideInterfaceSettingToggle />
+                            </Box>
+                        </RadioGroup>
+                    </FormControl>
+                    <Typography level="title-md" fontWeight="bold">
+                        {t("dialogues")}
+                    </Typography>
+                    <DialoguesSettings />
 
-            <ModalDialogCustom
-                open={openYouSure}
-                setOpen={setOpenYouSure}
-                color='danger'
-                head={<Typography level="h4"
-                    startDecorator={<ExitToAppIcon />}
-                >
-                    {t("attention")}
-                </Typography>}
-                actions={<>
-                    <Button
-                        key={'exit'}
-                        color='danger'
-                        variant="outlined"
-                        onClick={() => {
-                            gameEnd(navigate)
-                            setOpen(false)
-                            setOpenYouSure(false)
-                        }}
-                        startDecorator={<ExitToAppIcon />}
-                    >
-                        {t("exit")}
-                    </Button>
-                    <Button
-                        key={'cancel'}
-                        color="neutral"
-                        variant="plain"
-                        onClick={() => setOpenYouSure(false)}
-                    >
-                        {t("cancel")}
-                    </Button>
-                </>}
-            >
-                <Typography>
-                    {t("you_sure_to_return_main_menu")}
-                </Typography>
-            </ModalDialogCustom>
-        </>
+                    <Typography level="title-md" fontWeight="bold">
+                        {t("display")}
+                    </Typography>
+                    <FullScreenSettings />
+                    <ThemeSettings />
+                </DialogContent>
+                <ReturnMainMenuButton />
+            </Sheet>
+        </Drawer>
     );
 }
