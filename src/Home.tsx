dfbe9814d0ac192ff6835d512848ec5951da4159
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-01-14 09:36:38 +0100

    Refactor event handling by consolidating go back and close page detection into a custom hook for improved clarity and maintainability

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,17 +2,23 @@ import { Box } from '@mui/joy';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Routes from './AppRoutes';
 import useClosePageDetector from './hooks/useClosePageDetector';
-import useGoBackDetector from './hooks/useGoBackDetector';
 import useKeyboardDetector from './hooks/useKeyboardDetector';
+import useEventListener from './hooks/useKeyDetector';
 import Imports from './Imports';
 import GameSaveScreen from './screens/GameSaveScreen';
 import SaveLoadAlert from './screens/modals/SaveLoadAlert';
 import Settings from './screens/Settings';
 
 export default function Home() {
-    useGoBackDetector()
     useKeyboardDetector()
     useClosePageDetector()
+    // Prevent the user from going back to the previous page
+    useEventListener({
+        type: 'popstate',
+        listener: () => {
+            window.history.forward();
+        }
+    })
 
     return (
         <Imports>
