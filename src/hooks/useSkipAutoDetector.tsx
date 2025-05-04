d0fc0a57223138c65dcc5d291b1fc989f8a8e9be
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-05-04 17:03:33 +0200

    Add useInterval hook to useSkipAutoDetector for improved interval handling

--- src/hooks/useSkipAutoDetector.tsx
+++ src/hooks/useSkipAutoDetector.tsx
@@ -1,19 +1,20 @@
 import { narration } from "@drincs/pixi-vn";
 import { useQueryClient } from "@tanstack/react-query";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect } from "react";
+import { SKIP_DELAY } from "../constans";
 import useAutoInfoStore from "../stores/useAutoInfoStore";
 import useSkipStore from "../stores/useSkipStore";
 import useStepStore from "../stores/useStepStore";
 import useTypewriterStore from "../stores/useTypewriterStore";
 import { INTERFACE_DATA_USE_QUEY_KEY } from "../use_query/useQueryInterface";
 import useGameProps from "./useGameProps";
+import useInterval from "./useInterval";
 
 export default function useSkipAutoDetector() {
     const skipEnabled = useSkipStore((state) => state.enabled);
     const autoEnabled = useAutoInfoStore((state) => state.enabled);
     const autoTime = useAutoInfoStore((state) => state.time);
     const typewriterInProgress = useTypewriterStore((state) => state.inProgress);
-    const [recheckSkip, setRecheckSkip] = useState<number>(0);
     const setNextStepLoading = useStepStore((state) => state.setLoading);
     const queryClient = useQueryClient();
     const gameProps = useGameProps();
@@ -43,20 +44,10 @@ export default function useSkipAutoDetector() {
         }
     }, [gameProps, queryClient]);
 
-    useEffect(() => {
-        // Debouncing
-        let timeout = setTimeout(() => {
-            if (skipEnabled) {
-                nextOnClick().then(() => {
-                    setRecheckSkip((p) => p + 1);
-                });
-            }
-        }, 100);
-
-        return () => {
-            clearTimeout(timeout);
-        };
-    }, [skipEnabled, recheckSkip, nextOnClick]);
+    useInterval(nextOnClick, {
+        delay: SKIP_DELAY,
+        enabled: skipEnabled,
+    });
 
     useEffect(() => {
         if (skipEnabled) {
