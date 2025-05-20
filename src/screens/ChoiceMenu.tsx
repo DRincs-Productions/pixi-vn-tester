3ba362dea75608019897fcdf4fff475e091afe3a
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-05-20 11:18:10 +0200

    Refactor ChoiceMenu component to remove unused imports and simplify layout structure

--- src/screens/ChoiceMenu.tsx
+++ src/screens/ChoiceMenu.tsx
@@ -1,16 +1,14 @@
 import KeyboardReturnIcon from "@mui/icons-material/KeyboardReturn";
-import { Box, Grid } from "@mui/joy";
+import { Grid } from "@mui/joy";
 import { motion, Variants } from "motion/react";
 import ChoiceButton from "../components/ChoiceButton";
 import useNarrationFunctions from "../hooks/useNarrationFunctions";
-import useDialogueCardStore from "../stores/useDialogueCardStore";
 import useInterfaceStore from "../stores/useInterfaceStore";
 import useStepStore from "../stores/useStepStore";
 import { useQueryChoiceMenuOptions } from "../use_query/useQueryInterface";
 
-export default function ChoiceMenu({ fullscreen = true }: { fullscreen?: boolean }) {
+export default function ChoiceMenu() {
     const nextStepLoading = useStepStore((state) => state.loading);
-    const height = useDialogueCardStore((state) => 100 - state.height);
     const { data: menu = [] } = useQueryChoiceMenuOptions();
     const hidden = useInterfaceStore((state) => state.hidden || menu.length == 0);
     const { selectChoice } = useNarrationFunctions();
@@ -43,57 +41,46 @@ export default function ChoiceMenu({ fullscreen = true }: { fullscreen?: boolean
     };
 
     return (
-        <Box
+        <Grid
+            container
+            direction='column'
+            justifyContent='center'
+            alignItems='center'
+            spacing={2}
             sx={{
+                overflow: "auto",
+                height: "100%",
+                gap: 1,
                 width: "100%",
-                position: "absolute",
-                top: 0,
-                left: 0,
-                right: 0,
-                height: fullscreen ? "100%" : `${height}%`,
                 pointerEvents: hidden ? "none" : "auto",
             }}
+            component={motion.div}
+            variants={gridVariants}
+            animate={hidden ? "closed" : "open"}
         >
-            <Grid
-                container
-                direction='column'
-                justifyContent='center'
-                alignItems='center'
-                spacing={2}
-                sx={{
-                    overflow: "auto",
-                    height: "100%",
-                    gap: 1,
-                    width: "100%",
-                }}
-                component={motion.div}
-                variants={gridVariants}
-                animate={hidden ? "closed" : "open"}
-            >
-                {menu?.map((item, index) => {
-                    return (
-                        <Grid
-                            key={"choice-" + index}
-                            justifyContent='center'
-                            alignItems='center'
-                            component={motion.div}
-                            variants={itemVariants}
+            {menu?.map((item, index) => {
+                return (
+                    <Grid
+                        key={"choice-" + index}
+                        justifyContent='center'
+                        alignItems='center'
+                        component={motion.div}
+                        variants={itemVariants}
+                    >
+                        <ChoiceButton
+                            loading={nextStepLoading}
+                            onClick={() => selectChoice(item)}
+                            sx={{
+                                left: 0,
+                                right: 0,
+                            }}
+                            startDecorator={item.type == "close" ? <KeyboardReturnIcon /> : undefined}
                         >
-                            <ChoiceButton
-                                loading={nextStepLoading}
-                                onClick={() => selectChoice(item)}
-                                sx={{
-                                    left: 0,
-                                    right: 0,
-                                }}
-                                startDecorator={item.type == "close" ? <KeyboardReturnIcon /> : undefined}
-                            >
-                                {item.text}
-                            </ChoiceButton>
-                        </Grid>
-                    );
-                })}
-            </Grid>
-        </Box>
+                            {item.text}
+                        </ChoiceButton>
+                    </Grid>
+                );
+            })}
+        </Grid>
     );
 }
